728adfe6b16a96e82d07cd7999446b7b873d4d66
MartinSt23 <martin.staettner23@gmail.com>
2026-03-06 22:42:20 +0100

    feat: add Base Parser and refactor ApacheParser to class

--- src/siem/normalizer/apache_parser.py
+++ src/siem/normalizer/apache_parser.py
@@ -1,4 +1,5 @@
 import re
+from .base import BaseParser
 
 PATTERN= re.compile(
     r'(?P<ip>\S+) \S+ \S+ \[(?P<time>[^\]]+)\] '
@@ -6,15 +7,23 @@ PATTERN= re.compile(
     r'(?P<status>\d{3}) (?P<size>\S+)'
 )
 
-def parse(raw: str) -> dict | None:
-        m = PATTERN.match(raw)
-        if not m:
-            return None
-        return {
-              "source_ip": m.group("ip"),
-              "timestamp": m.group("time"),
-              "method": m.group("method"),
-              "path": m.group("path"),
-              "status": int(m.group("status")),
-              "size": m.group("size"),
-        }
+class ApacheParser(BaseParser):
+    def parse(self, raw: str) -> dict | None:
+            m = PATTERN.match(raw)
+            if not m:
+                return None
+            return {
+                "source_ip": m.group("ip"),
+                "timestamp": m.group("time"),
+                "method": m.group("method"),
+                "path": m.group("path"),
+                "status": int(m.group("status")),
+                "size": m.group("size"),
+                "event_type": self._classify(int(m.group("status"))),
+            }
+
+    def _classify(self, status: int) -> str:
+        if status == 401: return "LOGIN_FAILED"
+        if status >= 500: return "SERVER_ERROR"
+        if status >= 400: return "CLIENT_ERROR"
+        return "SUCCESS"
